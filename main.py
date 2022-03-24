e6c094e189b6a10f14cd851ea9a6f04b4c1e2465
ShreyasAgnihotri <98114400+ShreyasAgnihotri@users.noreply.github.com>
2022-03-24 19:29:56 +0530

    Update main.py
    Added the feature of taking audio file destination as user input and format of audio as user input as well

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
 from pydub import AudioSegment
+import os
 
-counter = 0
 flag = True
 
+counter = 0
+    
+
+def mp3(t1,t2,user_input):
+        global flag,counter
+        newAudio = AudioSegment.from_mp3(user_input)
+        newAudio = newAudio[t1:t2]
+        newAudio.export(f'newSample{counter}.mp3', format="mp3")
+        counter += 1
+        choice = input("Do You want to continue Splitting: (Y/N) ").lower()
+        if choice == "y":
+            flag = True
+        else:
+            flag = False
+
+def wav(t1,t2,user_input):
+        global flag, counter
+        newAudio = AudioSegment.from_wav(user_input)
+        newAudio = newAudio[t1:t2]
+        newAudio.export(f'newSample{counter}.wav', format="wav")
+        counter += 1
+        choice = input("Do You want to continue Splitting: (Y/N) ").lower()
+        if choice == "y":
+            flag =  True
+        else:
+            flag = False
+
+
 while flag:
-    t1 = int(input("Enter the Starting time in seconds to split: "))
-    t2 = int(input("Enter the Ending time in seconds to split: "))
-    t1 = t1 * 1000 
-    t2 = t2 * 1000
-    newAudio = AudioSegment.from_wav("Sample.wav")
-    newAudio = newAudio[t1:t2]
-    newAudio.export(f'newSample{counter}.wav', format="wav")
-    counter += 1
-    choice = input("Do You want to continue Splitting: (Y/N) ").lower()
-    if choice == "y":
-        flag = True
+    
+    format = input("Enter the format of your audio file: (mp3/wav) ").lower()
+    if format == "mp3":
+        user_input = input("Enter the path of your file: ")
+        t1 = int(input("Enter the Starting time in seconds to split: "))
+        t2 = int(input("Enter the Ending time in seconds to split: "))
+        t1 = t1 * 1000 
+        t2 = t2 * 1000
+        assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
+        mp3(t1,t2,user_input)
+        
+    elif format == "wav":
+        user_input = input("Enter the path of your file: ")
+        t1 = int(input("Enter the Starting time in seconds to split: "))
+        t2 = int(input("Enter the Ending time in seconds to split: "))
+        t1 = t1 * 1000 
+        t2 = t2 * 1000
+        assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
+        wav(t1,t2,user_input)
+        
     else:
-        flag = False
+        print("Invalid Format")
+        flag = True
